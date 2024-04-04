5f9a98888e6923e3ebd869b584c1b2ab9e1b28fc
Screenbones <whoami@kotsu.red>
2024-04-04 13:58:47 -0700

    Day 35: Visualize Your Mood Across Days Student Project

---
+++ Home.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import glob
+from nltk.sentiment import SentimentIntensityAnalyzer
+import plotly.express as px
+from pathlib import Path
+
+
+filepaths = sorted(glob.glob("diary/*.txt"))
+diary = []
+for path in filepaths:
+    with open(path, "r") as entry:
+        diary.append(entry.read())
+
+
+diarytone = []
+analyzer = SentimentIntensityAnalyzer()
+for entry in diary:
+    diarytone.append(analyzer.polarity_scores(entry))
+
+
+positive_scores = []
+for tone in diarytone:
+    positive_scores.append(tone["pos"])
+
+
+negative_scores = []
+for tone in diarytone:
+    negative_scores.append(tone["neg"])
+
+
+dates = [Path(name).stem for name in filepaths]
+labels = {"x": "Date", "y": "Score"}
+
+
+st.title("Diary Tone")
+
+st.header("Positivity")
+positive_graph = px.line(x=dates, y=positive_scores, labels=labels)
+st.plotly_chart(positive_graph)
+
+st.header("Negativity")
+negative_graph = px.line(x=dates, y=negative_scores, labels=labels)
+st.plotly_chart(negative_graph)
